2c117eb897da5493770d943abd4a568e87ba98ee
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-06-20 19:03:31 -0300

    Adiciona função de envio de informações ao local storage

--- src/helpers/LocalStorage.js
+++ src/helpers/LocalStorage.js
@@ -9,3 +9,12 @@ export const cocktailsToLocalStorage = () => {
 export const userEmailToLocalStorage = (value) => {
   localStorage.setItem('user', JSON.stringify({ email: value }));
 };
+
+// export const infoPlayerToLocalStorage = (player) => {
+//   if (JSON.parse(localStorage.getItem('ranking'))) {
+//     const ranking = JSON.parse(localStorage.getItem('ranking'));
+//     localStorage.setItem('ranking', JSON.stringify([...ranking, player]));
+//   } else {
+//     localStorage.setItem('ranking', JSON.stringify([player]));
+//   }
+// };
