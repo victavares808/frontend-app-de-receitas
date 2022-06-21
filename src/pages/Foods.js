239e2ab147bc6f119739174f8d6db20f65aa9b34
vicdev <vtavares.tur@gmail.com>
2022-06-21 19:13:30 -0300

    cria componente Detalhes de receita e importação do mesmo para páginas /foods e /drinks

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FooterComponent from '../components/FooterComponent';
 import HeaderComponent from '../components/HeaderComponet';
+import RecipeDetaisComponent from '../components/RecipeDetaisComponent';
 
 const Foods = () => (
   <div>
     <HeaderComponent pageTitle="Foods" />
     <FooterComponent />
+    <RecipeDetaisComponent />
   </div>
 );
 
